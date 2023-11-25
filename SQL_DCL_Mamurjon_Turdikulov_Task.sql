2552d5cd3cccc1b6dcaf80e379654e95277ec39e
MamurTurdikulov <148514012+MamurTurdikulov@users.noreply.github.com>
2023-11-25 12:37:17 +0500

    Update SQL_DCL_Mamurjon_Turdikulov_Task.sql

--- SQL_DCL_Mamurjon_Turdikulov_Task.sql
+++ SQL_DCL_Mamurjon_Turdikulov_Task.sql
@@ -1,26 +1,39 @@
+-- Create user and grant connect
 CREATE USER rentaluser WITH PASSWORD 'rentalpassword';
 GRANT CONNECT ON DATABASE dvd_rental TO rentaluser;
 
+-- Grant SELECT permission for the "customer" table
 GRANT SELECT ON TABLE customer TO rentaluser;
 
+-- Create group and add user to the group
 CREATE GROUP rental;
 ALTER USER rentaluser IN GROUP rental;
 
+-- Grant INSERT and UPDATE permissions for the "rental" table to the "rental" group
 GRANT INSERT, UPDATE ON TABLE rental TO rental;
 
+-- Insert and update rows in the "rental" table under the "rental" group
 INSERT INTO rental (rental_date, inventory_id, customer_id, return_date) 
 VALUES ('2023-11-25', 1, 1, NULL);
 UPDATE rental SET return_date = '2023-12-01' WHERE rental_id = 1;
 
+-- Revoke INSERT permission for the "rental" group on the "rental" table
 REVOKE INSERT ON TABLE rental FROM rental;
 
-CREATE ROLE client_first_name_last_name;
-GRANT USAGE, SELECT ON TABLE rental TO client_first_name_last_name;
-GRANT USAGE, SELECT ON TABLE payment TO client_first_name_last_name;
+-- Create personalized roles for existing customers
+DO $$ 
+DECLARE 
+    customer_record record;
+BEGIN 
+    FOR customer_record IN SELECT customer_id, first_name, last_name FROM customer LOOP
+        EXECUTE 'CREATE ROLE client_' || customer_record.first_name || '_' || customer_record.last_name;
+        EXECUTE 'GRANT USAGE, SELECT ON TABLE rental TO client_' || customer_record.first_name || '_' || customer_record.last_name;
+        EXECUTE 'GRANT USAGE, SELECT ON TABLE payment TO client_' || customer_record.first_name || '_' || customer_record.last_name;
+        EXECUTE 'GRANT client_' || customer_record.first_name || '_' || customer_record.last_name || ' TO ' || customer_record.customer_id;
+    END LOOP;
+END $$;
 
-GRANT client_first_name_last_name TO {customer_id};
-
+-- Query to make sure a user sees only their own data in the "rental" table
 SET ROLE client_first_name_last_name;
 SELECT * FROM rental WHERE customer_id = {customer_id};
-SELECT * FROM payment WHERE customer_id = {customer_id};
 RESET ROLE;
